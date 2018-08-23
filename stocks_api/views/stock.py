730bddd656978292498582336f7318f0f43e1817
Ben Hurst <BHurst8@gmail.com>
2018-08-23 09:22:35 -0700

    Completed all routes

--- stocks_api/views/stock.py
+++ stocks_api/views/stock.py
@@ -4,10 +4,17 @@ import json
 
 
 class StockAPIView(APIViewSet):
+    def retrieve(self, request, id):
+        unique_id = id
+        return Response(json={'company': f'''Here's the id for that company... {unique_id}'''}, status=200)
+
     def create(self, request):
         my_database_is_a_variable = request.body
         name = json.loads(my_database_is_a_variable)['name']
         symbol = json.loads(my_database_is_a_variable)['symbol']
         portfolio_id = json.loads(my_database_is_a_variable)['portfolio_id']
         return Response(json=f'''{{'message': 'Stock record for Portfolio ID#{portfolio_id} created for {name} (Ticker Symbol: {symbol})'}}''', status=201)
+
+    def destroy(self, request, id):
+        return Response(json={'message': 'Successfully deleted the record for ID#{id}'}, status=204)
 
